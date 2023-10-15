ed7a2633549d8feb08435265cb3b8611773a432d
Adrian <256286@student.pwr.edu.pl>
2023-10-15 12:00:17 +0200

    added user unlimited tablespace

--- create_tables.sql
+++ create_tables.sql
@@ -3,14 +3,14 @@ create table AgeRestriction
     age_restriction_name varchar2(50) primary key
 );
 
-create table User_
+create table CinemaUser
 (
     id_user  number primary key,
     name     varchar2(10) not null,
     surname  varchar2(10) not null,
-    email    varchar2(20) not null,
+    email    varchar2(50) not null,
     password varchar2(20) not null,
-    brith    date not null
+    birth    date not null
 );
 
 create table Room
@@ -18,49 +18,49 @@ create table Room
     id_room                 number primary key,
     room_sign               varchar2(20) not null,
     floor_number            number not null,
-    imax_capable            varchar2(1) check (imax_capable in ('T', 'F')) not null,
-    wheelchair_availability varchar2(1) check (wheelchair_availability in ('T', 'F')) not null,
+    imax_capable            varchar2(1) check (imax_capable in ('Y', 'N')) not null,
+    wheelchair_availability varchar2(1) check (wheelchair_availability in ('Y', 'N')) not null,
     sponsor                 varchar2(50),
-    is_3D_capable           varchar2(1) check (is_3D_capable in ('T', 'F')) not null
+    capable_3D           varchar2(1) check (capable_3D in ('Y', 'N')) not null
 );
 
 create table Language
 (
-    code varchar2(10) primary key,
-    name varchar2(50) not null
+    id_language varchar2(10) primary key,
+    language_name varchar2(50) not null
 );
 
 create table Movie
 (
     id_movie           number primary key,
     name               varchar2(100) not null,
-    premiere_date      date,
+    premiere      date,
     duration           number not null,
-    is_imax            varchar2(1) check (is_imax in ('T', 'F')) not null,
+    is_imax            varchar2(1) check (is_imax in ('Y', 'N')) not null,
     fk_age_restriction varchar2(50),
     fk_language        varchar2(10),
 
     foreign key (fk_age_restriction)
         references AgeRestriction (age_restriction_name),
     foreign key (fk_language)
-        references Language (code)
+        references Language (id_language)
 );
 
 create table MovieVersion
 (
     id_movie_version       number primary key,
-    dimension              varchar2(2) check ( dimension in ('2D', '3D') ) not null,
+    dimension              varchar2(10) not null,
     fk_subtitles_language  varchar2(10),
     fk_dubbing_language    varchar2(10),
     fk_voice_over_language varchar2(10),
     fk_movie               number,
 
     foreign key (fk_subtitles_language)
-        references Language (code),
+        references Language (id_language),
     foreign key (fk_dubbing_language)
-        references Language (code),
+        references Language (id_language),
     foreign key (fk_voice_over_language)
-        references Language (code),
+        references Language (id_language),
     foreign key (fk_movie)
         references Movie (id_movie)
 );
@@ -85,7 +85,7 @@ create table Seat
     id_seat           number primary key,
     seat_row          number not null,
     seat_number       number not null,
-    is_VIP_seat       varchar2(1) check ( is_VIP_seat in ('T', 'F') ) not null,
+    is_vip_seat       varchar2(1) check ( is_VIP_seat in ('Y', 'N') ) not null,
     fk_room           number,
     fk_connected_seat number,
 
@@ -99,7 +99,7 @@ create table Ticket
 (
     id_ticket number primary key,
     discount  float,
-    price     float not null,
+    base_price     float not null,
     purchase  timestamp,
     fk_seat   number,
     fk_show   number,
@@ -110,5 +110,7 @@ create table Ticket
     foreign key (fk_show)
         references Show (id_show),
     foreign key (fk_user)
-        references User_ (id_user)
+        references CinemaUser (id_user)
 );
+
+ALTER SESSION SET NLS_DATE_FORMAT = 'YYYY-MM-DD';
--- create_user.sql
+++ create_user.sql
@@ -1,2 +1,3 @@
 create user test identified by test;
 grant connect, resource to test;
+grant unlimited tablespace to test;
