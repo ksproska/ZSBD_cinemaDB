2d6dcdc832c8e310a5b425d19fc8ebedca4a6208
Adrian <256286@student.pwr.edu.pl>
2023-10-26 17:51:16 +0200

    added makefile

--- db_generation/timmer.sql
+++ db_generation/timmer.sql
@@ -1,2 +1,3 @@
 select LAST_LOAD_TIME, ELAPSED_TIME, MODULE, SQL_TEXT elapsed from v$sql
-  order by LAST_LOAD_TIME desc
+  order by LAST_LOAD_TIME desc;
+select count(*) from Tickets;
---
+++ db_generation/volume_data/export.sh
@@ -0,0 +1,1 @@
+expdp system/test schemas=TEST directory=DATA_PUMP_DIR dumpfile=TEST.dmp logfile=expdpTEST.log
