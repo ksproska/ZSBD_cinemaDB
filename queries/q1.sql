1ec49793afb5cc7f8ce9de7a5efc09e5c92d5ab4
Kamila Sproska <kamila.sproska@gmail.com>
2023-11-07 14:29:57 +0100

    fix for q1

--- queries/q1.sql
+++ queries/q1.sql
@@ -1,14 +1,36 @@
-WITH SELECTED_SHOW(THE_SHOW) AS (
-    SELECT ID_SHOW AS THE_SHOW FROM SHOWS JOIN ROOMS on ROOMS.ID_ROOM = SHOWS.FK_ROOM
-    WHERE
-        TO_DATE(:show_date, 'yyyy-mm-dd') + NUMTODSINTERVAL(:start_hour, 'HOUR') + NUMTODSINTERVAL(:start_minutes,'MINUTE') < SHOWS.SHOW_DATE + NUMTODSINTERVAL(START_HOUR, 'HOUR') + NUMTODSINTERVAL(START_MINUTE, 'MINUTE')
-        AND TO_DATE(:show_date, 'yyyy-mm-dd') + NUMTODSINTERVAL(:start_hour, 'HOUR') + NUMTODSINTERVAL(:start_minutes,'MINUTE') + NUMTODSINTERVAL(:seek_length_hours, 'HOUR') > SHOWS.SHOW_DATE + NUMTODSINTERVAL(START_HOUR, 'HOUR') + NUMTODSINTERVAL(START_MINUTE,'MINUTE')
-    --WHERE ID_SHOW = :show_id
-)
-SELECT SEAT_ROW, SEAT_NUMBER, IS_VIP_SEAT, (CASE WHEN (FK_CONNECTED_SEAT IS NOT NULL) THEN 'Y' ELSE 'N' END) AS IS_DOUBLE_SEAT
-FROM SEATS
-    JOIN ROOMS ON SEATS.FK_ROOM = ROOMS.ID_ROOM
-    JOIN SHOWS ON ROOMS.ID_ROOM = SHOWS.FK_ROOM
-WHERE
-    ID_SEAT NOT IN (SELECT FK_SEAT FROM TICKETS WHERE FK_SHOW IN (SELECT * FROM SELECTED_SHOW))
-  AND ID_SHOW IN (SELECT * FROM SELECTED_SHOW);
+-- Wyświetl wszystkie wolne miejsca na seanse
+-- rozpoczynające się w od danej godziny i minuty do max zadanego przedziału w godzinach
+WITH SELECTED_SHOWS(ID_SHOW, ID_ROOM, START_TIME, ID_SEAT, SEAT_ROW, SEAT_NUMBER, IS_VIP_SEAT, IS_DOUBLE_SEAT, ID_TICKET)
+         AS (SELECT ID_SHOW                                                           AS ID_SHOW,
+                    ID_ROOM,
+                    SHOWS.SHOW_DATE + NUMTODSINTERVAL(START_HOUR, 'HOUR') +
+                    NUMTODSINTERVAL(START_MINUTE, 'MINUTE')                           AS START_TIME,
+                    ID_SEAT,
+                    SEAT_ROW,
+                    SEAT_NUMBER,
+                    IS_VIP_SEAT,
+                    (CASE WHEN (FK_CONNECTED_SEAT IS NOT NULL) THEN 'Y' ELSE 'N' END) AS IS_DOUBLE_SEAT,
+                    ID_TICKET
+             FROM SHOWS
+                      JOIN ROOMS on ROOMS.ID_ROOM = SHOWS.FK_ROOM
+                      JOIN SEATS S on ROOMS.ID_ROOM = S.FK_ROOM
+                      LEFT JOIN TICKETS on S.ID_SEAT = TICKETS.FK_SEAT AND SHOWS.ID_SHOW = TICKETS.FK_SHOW
+             WHERE TO_DATE(:show_date, 'yyyy-mm-dd') + NUMTODSINTERVAL(:start_hour, 'HOUR') +
+                   NUMTODSINTERVAL(:start_minutes, 'MINUTE') <=
+                   SHOWS.SHOW_DATE + NUMTODSINTERVAL(START_HOUR, 'HOUR') +
+                   NUMTODSINTERVAL(START_MINUTE, 'MINUTE')
+               AND TO_DATE(:show_date, 'yyyy-mm-dd') + NUMTODSINTERVAL(:start_hour, 'HOUR') +
+                   NUMTODSINTERVAL(:start_minutes, 'MINUTE') +
+                   NUMTODSINTERVAL(:seek_length_hours, 'HOUR') >=
+                   SHOWS.SHOW_DATE + NUMTODSINTERVAL(START_HOUR, 'HOUR') +
+                   NUMTODSINTERVAL(START_MINUTE, 'MINUTE')
+             ORDER BY START_TIME, ID_SHOW, SEAT_ROW, SEAT_NUMBER)
+SELECT ID_SHOW,
+       START_TIME,
+       SEAT_ROW,
+       SEAT_NUMBER,
+       IS_VIP_SEAT,
+       IS_DOUBLE_SEAT
+FROM SELECTED_SHOWS
+WHERE ID_TICKET IS NOT NULL
+ORDER BY START_TIME, ID_SHOW, SEAT_ROW, SEAT_NUMBER;
