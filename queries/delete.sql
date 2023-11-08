4f0d0ea973df31ee32d7ca655cd5a8a36cc63a54
Kamila Sproska <kamila.sproska@gmail.com>
2023-11-08 22:06:20 +0100

    delete is working but deletes not many records

--- queries/delete.sql
+++ queries/delete.sql
@@ -1,10 +1,20 @@
 -- Usuń wszystkich użytkowników, którzy nie byli na żadnym seansie od określonego czasu (np. 2 lata)
 -- i nie mają żadnych aktualnych biletów (czyli takich, na które seans się jeszcze nie odbył)
--- :cutoff_date = '2023-08-01'
-DELETE
-FROM CINEMAUSERS
-WHERE ID_USER NOT IN (SELECT FK_USER
-                      FROM TICKETS
-                               JOIN SHOWS S on TICKETS.FK_SHOW = S.ID_SHOW
-                      WHERE SHOW_DATE < TO_DATE(:cutoff_date, 'yyyy-mm-dd')
-                        AND FK_USER IS NOT NULL);
+-- :cutoff_date = '2024-01-17'
+BEGIN
+    DELETE
+    FROM TICKETS
+    WHERE FK_USER NOT IN (SELECT FK_USER
+                          FROM TICKETS
+                                   JOIN SHOWS on TICKETS.FK_SHOW = SHOWS.ID_SHOW
+                          WHERE TO_DATE(:cutoff_date, 'yyyy-mm-dd') < SHOW_DATE
+                            AND FK_USER IS NOT NULL GROUP BY FK_USER);
+    DELETE
+    FROM CINEMAUSERS
+    WHERE ID_USER NOT IN (SELECT FK_USER
+                          FROM TICKETS
+                                   JOIN SHOWS on TICKETS.FK_SHOW = SHOWS.ID_SHOW
+                          WHERE TO_DATE(:cutoff_date, 'yyyy-mm-dd') < SHOW_DATE
+                            AND FK_USER IS NOT NULL);
+    commit;
+END;
