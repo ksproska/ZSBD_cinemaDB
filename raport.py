28b71bc8f1477a1cd2b6ef9d5d7f81170b810852
adrian <256286@student.pwr.edu.pl>
2024-01-03 17:50:45 +0100

    raport.py readability improvement

--- raport.py
+++ raport.py
@@ -2,10 +2,12 @@ import re
 import sys
 
 names = ['q1', 'q2', 'q3', 'q4', 'update', 'insert', 'delete']
+pattern_find = r'(?s)SQL_ID.*?selected\.'
+pattern_delete = r'\nPLAN_TABLE_OUTPUT.*\n-*\n'
 
 res = ''.join(sys.stdin.readlines())
 
-raport_list = re.findall(r'(?s)SQL_ID.*?selected\.', res)
+raport_list = re.findall(pattern_find, res)
 raport = '\n\n'.join(['\n\n'.join(i) for i in zip(names, raport_list)])
 
-print(re.sub(r'\nPLAN_TABLE_OUTPUT.*\n-*\n', '', raport))
+print(re.sub(pattern_delete, '', raport))
